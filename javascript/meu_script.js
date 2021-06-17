ee934386d8402649c486697d2c4232ac5192ebfd
Henrique-Arau <83648293+Henrique-Arau@users.noreply.github.com>
2021-06-17 14:01:37 -0300

    Javascript
    Conteúdos de Estudos para Javascript.

---
+++ javascript/meu_script.js
@@ -0,0 +1,7 @@
+//alert('olá, seja bem vindo ao curso!!!')
+
+/*
+         selecionar um elemento do DOM
+         atualizar o valor elemento com uma string
+*/
+document.getElementById('nome').value ='oi'
